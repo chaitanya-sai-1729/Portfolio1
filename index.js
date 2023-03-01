2678e865c45f920f826d0db396d2170a16319745
Chaitanya <chaitanyasai.m.5@gmail.com>
2023-03-01 20:50:19 +0530

    seventh commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname+"/public"));
 app.listen(process.env.PORT||3000, function() {
   console.log("server is up and running at 3000")
 });
