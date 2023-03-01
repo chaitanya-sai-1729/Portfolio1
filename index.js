c8a509d7e008d108c4739fdfeb23eac9929e8d68
Chaitanya <chaitanyasai.m.5@gmail.com>
2023-03-01 20:46:53 +0530

    seventh commit

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml12');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+const express = require("express");
+const app = express();
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml12 .letter',
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1200,
-    delay: (el, i) => 500 + 30 * i
-  }).add({
-    targets: '.ml12 .letter',
-    translateX: [0,-30],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 1100,
-    delay: (el, i) => 100 + 30 * i
-  });
-  // For Skills
+app.use(express.static("public"));
+app.listen(process.env.PORT||3000, function() {
+  console.log("server is up and running at 3000")
+});
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html")
+});
 
-  
-  //For Contact me
+app.get("/certificates",function(req,res){
+  res.sendFile(__dirname+"/certificates.html")
+})
+app.get("/contactme",function(req,res){
+  res.sendFile(__dirname+"/contactme.html")
+})
